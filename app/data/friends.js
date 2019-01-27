8a5ae54e78b7b8e1738e749ecf4afa72e52e5ea5
bareseph <barryriveroll@gmail.com>
2019-01-27 17:22:14 -0500

    Add route functionality, initial data placeholder, and initial survey.html structure

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+var friendsArray = [
+  {
+    name: "Walter",
+    photo: "",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Paul",
+    photo: "",
+    scores: [2, 3, 4, 1, 2, 5, 5, 2, 3, 1]
+  }
+];
+
+module.exports = friendsArray;
