12a5173c5006045867e62c752361f670f0e4ed0a
bareseph <barryriveroll@gmail.com>
2019-01-31 17:02:40 -0500

    Update friends data and update home.html

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,53 @@
 var friendsArray = [
   {
     name: "Walter",
-    photo: "",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    photo: "https://i.imgur.com/o5etUtJ.jpg",
+    scores: [2, 5, 5, 5, 1, 2, 3, 1, 2, 4]
   },
   {
     name: "Paul",
-    photo:
-      "https://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg",
+    photo: "https://i.imgur.com/tcRYEdk.jpg",
     scores: [2, 3, 4, 1, 2, 5, 5, 2, 3, 1]
+  },
+  {
+    name: "Francine",
+    photo: "https://i.imgur.com/EeYomjb.jpg",
+    scores: [2, 3, 5, 5, 2, 5, 1, 1, 3, 4]
+  },
+  {
+    name: "Sundar",
+    photo: "https://i.imgur.com/rCgIgrD.jpg",
+    scores: [4, 4, 2, 3, 2, 2, 5, 3, 3, 3]
+  },
+  {
+    name: "Hector",
+    photo: "https://i.imgur.com/HSfSQs1.jpg",
+    scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+  },
+  {
+    name: "Hailey",
+    photo: "https://i.imgur.com/EsXOAgn.jpg",
+    scores: [4, 5, 4, 3, 2, 1, 1, 1, 1, 1]
+  },
+  {
+    name: "Janie",
+    photo: "https://i.imgur.com/pMnUe91.jpg",
+    scores: [2, 2, 4, 1, 1, 1, 5, 5, 5, 1]
+  },
+  {
+    name: "Madeline",
+    photo: "https://i.imgur.com/ErnagEu.jpg",
+    scores: [3, 3, 1, 3, 2, 4, 5, 1, 2, 1]
+  },
+  {
+    name: "Aisha",
+    photo: "https://i.imgur.com/yGW89Xj.jpg",
+    scores: [5, 5, 5, 4, 5, 3, 2, 1, 4, 5]
+  },
+  {
+    name: "Pauline",
+    photo: "https://i.imgur.com/7VfFmyG.jpg",
+    scores: [1, 2, 3, 4, 5, 1, 1, 1, 2, 1]
   }
 ];
 
